ca778ebb32966f9c1cad12fe121394e578a93a61
PV <2190926+pvaladares@users.noreply.github.com>
2022-04-09 19:56:40 +0100

    revised version

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,10 +6,6 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    /*
-     * We will be using this below to help generate a random number
-     */
     uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -22,6 +18,12 @@ contract WavePortal {
 
     Wave[] waves;
 
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log("We have been constructed!");
         /*
@@ -31,6 +33,19 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 45-seconds bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 45 seconds < block.timestamp,
+            "Must wait 45 seconds before waving again."
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
@@ -41,21 +56,13 @@ contract WavePortal {
          */
         seed = (block.difficulty + block.timestamp + seed) % 100;
 
-        console.log("Random # generated: %d", seed);
-
-        /*
-         * Give a 50% chance that the user wins the prize.
-         */
         if (seed <= 50) {
             console.log("%s won!", msg.sender);
 
-            /*
-             * The same code we had before to send the prize.
-             */
             uint256 prizeAmount = 0.0001 ether;
             require(
                 prizeAmount <= address(this).balance,
-                "Trying to withdraw more money than the contract has."
+                "Trying to withdraw more money than they contract has."
             );
             (bool success, ) = (msg.sender).call{value: prizeAmount}("");
             require(success, "Failed to withdraw money from contract.");
